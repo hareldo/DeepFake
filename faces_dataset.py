207bd059025cc54f4302da75e6ee268dd7e43b25
hareldo <44146957+hareldo@users.noreply.github.com>
2023-02-26 15:37:06 +0200

    Add files via upload

---
+++ faces_dataset.py
@@ -0,0 +1,49 @@
+"""Custom faces dataset."""
+import os
+
+import torch
+from PIL import Image
+from torch.utils.data import Dataset
+import torchvision.transforms as transforms
+
+
+class FacesDataset(Dataset):
+    """Faces dataset.
+
+    Attributes:
+        root_path: str. Directory path to the dataset. This path has to
+        contain a subdirectory of real images called 'real' and a subdirectory
+        of not-real images (fake / synthetic images) called 'fake'.
+        transform: torch.Transform. Transform or a bunch of transformed to be
+        applied on every image.
+    """
+    def __init__(self, root_path: str, transform=None):
+        """Initialize a faces dataset."""
+        self.root_path = root_path
+        self.real_image_names = os.listdir(os.path.join(self.root_path, 'real'))
+        self.fake_image_names = os.listdir(os.path.join(self.root_path, 'fake'))
+        self.transform = transform
+
+    def __getitem__(self, index) -> tuple[torch.Tensor, int]:
+        """Get a sample and label from the dataset."""
+
+        if index < len(self.real_image_names):
+            image_path = os.path.join(self.root_path, 'real', self.real_image_names[index])
+            label = 0
+        else:
+            image_path = os.path.join(self.root_path, 'fake',
+                                      self.fake_image_names[index - len(self.real_image_names)])
+            label = 1
+
+        image = Image.open(image_path)
+        if self.transform is not None:
+            tensor_image = self.transform(image)
+        else:
+            converter = transforms.PILToTensor()
+            tensor_image = converter(image)
+
+        return tensor_image, label
+
+    def __len__(self):
+        """Return the number of images in the dataset."""
+        return len(self.real_image_names) + len(self.fake_image_names)
